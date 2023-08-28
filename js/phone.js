ad5a33ecc929e3039ecb396369a54db837bfaa34
emran <shuvoorba28@gmail.com>
2023-08-28 17:13:10 +0600

    dynamically phone loading done

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const showPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const showPhone = async (phone) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${phone}`)
     const data = await res.json();
     phoneManager(data.data);
 }
@@ -10,16 +10,15 @@ const phoneManager = (phones) => {
     const phoneContainer = document.getElementById('card-container');
     phones.forEach(phone => {
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.classList = `card w-5/6 bg-gray-200 mt-4 shadow-xl`;
         phoneCard.innerHTML = `
                     <figure class="px-10 pt-10">
                         <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
                     </figure>
                     <div class="card-body items-center text-center">
-                        <h2 class="card-title">${phone.phone_name}</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 class="card-title mt-2">${phone.phone_name}</h2>
                         <div class="card-actions">
-                            <button class="btn btn-primary">Buy Now</button>
+                            <button class="btn mt-2 btn-primary">Buy Now</button>
                         </div>
                     </div>
          `;
@@ -28,5 +27,11 @@ const phoneManager = (phones) => {
     console.log(phones)
 }
 
+
+document.getElementById('search-button').addEventListener('click', function(){
+    const searchElement = document.getElementById('search-field');
+    const searchValue = searchElement.value;
+    showPhone(searchValue);
+})
 
 showPhone();
